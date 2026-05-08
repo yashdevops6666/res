f111b1a01afa85231c56e18d397f8475f603b250
Yashwanth Keerthi <iamkeerthiyashwanth@gmail.com>
2026-05-08 21:24:43 +0100

    Initial commit

---
+++ resume-builder/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`ResumeForge running on port ${PORT}`);
+});
